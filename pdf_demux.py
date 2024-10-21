e758cf760955bc6cd92003cda24f01d761fdd863
Rolando Garcia <rolando.garcia@asu.edu>
2024-10-21 11:46:34 -0500

    ocr with doctr

--- pdf_demux.py
+++ pdf_demux.py
@@ -1,100 +1,50 @@
 import fitz
 import os
 from PIL import Image
-import pytesseract
-from multiprocessing import Pool
 
 from app.constants import DOC_DIR
-from tqdm import tqdm
 import io
 import time
 
+import flor
+import numpy as np
+from doctr.io import DocumentFile
+from doctr.models import ocr_predictor
 
-def resize_image(image_path, size=(300, 400)):
-    # Open an image file
-    with Image.open(image_path) as img:
-        # Resize the image
-        img = img.resize(size, Image.LANCZOS)
-        # Save the image back to the same path
-        img.save(image_path)
+model = ocr_predictor(
+    det_arch="linknet_resnet50", reco_arch="master", pretrained=True
+).to("cuda")
 
-
-def process_page(pdf_path, page_num, img_path, txt_path, ocr_path):
+pdf_files = [each for each in os.listdir(DOC_DIR) if each.endswith(".pdf")]
+for pdf_file in flor.loop("document", pdf_files):
+    pdf_path = os.path.join(DOC_DIR, pdf_file)
     base, ext = os.path.splitext(pdf_path)
-    doc = fitz.open(pdf_path)  # type: ignore
-    page = doc.load_page(page_num)
-
-    pix = page.get_pixmap()
-    output_image = os.path.join(img_path, f"page_{page_num}.png")
-    pix.save(output_image)
-    img_bytes = io.BytesIO(pix.tobytes("png"))
-    img = Image.open(img_bytes)
-
-    if page_num == 0:
-        # Save the first page as the preview
-        preview_path = os.path.join(base, "preview.png")
-        pix.save(preview_path)
-        resize_image(preview_path)
-
-    # Extract text and save as TXT
-    text = page.get_text()
-    output_text = os.path.join(txt_path, f"page_{page_num}.txt")
-    with open(output_text, "w", encoding="utf-8") as text_file:
-        text_file.write(text)
-
-    # Extract text with pytesseract
-    extracted_text = pytesseract.image_to_string(img)
-
-    # Save the extracted text
-    ocr_file_path = os.path.join(ocr_path, f"page_{page_num}.txt")
-    with open(ocr_file_path, "w", encoding="utf-8") as text_file:
-        text_file.write(extracted_text)
-
-    doc.close()
-
-
-def process_pdf(pdf_path, all_args):
-    # set img_path, txt_path, and ocr_path
-    base, ext = os.path.splitext(pdf_path)
-
     img_path = os.path.join(base, "images")
     os.makedirs(img_path, exist_ok=True)
+    doc = fitz.open(pdf_path)
+    doctr_doc = DocumentFile.from_pdf(pdf_path)
+    for page_num in flor.loop("page", range(doc.page_count)):
+        page = doc.load_page(page_num)
+        # Extract text and save as TXT
+        flor.log("plain_text", page.get_text())
 
-    txt_path = os.path.join(base, "txt")
-    os.makedirs(txt_path, exist_ok=True)
+        pix = page.get_pixmap()
+        output_image = os.path.join(img_path, f"page_{page_num}.png")
+        pix.save(output_image)
 
-    ocr_path = os.path.join(base, "ocr")
-    os.makedirs(ocr_path, exist_ok=True)
+        img_bytes = io.BytesIO(pix.tobytes("png"))
+        img = Image.open(img_bytes)
 
-    doc = fitz.open(pdf_path)  # type: ignore
-    page_numbers = range(len(doc))
+        if page_num == 0:
+            # Save the first page as the preview
+            preview_path = os.path.join(base, "preview.png")
+            img = img.resize((300, 400), Image.LANCZOS)
+            img.save(preview_path)
+
+        # Extract text with doctr
+        result = model(doctr_doc[page_num : page_num + 1])
+        flor.log("ocr_text", result.render())
+
     doc.close()
 
-    args = [
-        (pdf_path, page_num, img_path, txt_path, ocr_path) for page_num in page_numbers
-    ]
-    all_args.extend(args)
-
-
-if __name__ == "__main__":
-    all_args = []
-
-    cpu_count = os.cpu_count()
-    if cpu_count is None or cpu_count < 4:
-        max_workers = 1
-    else:
-        max_workers = 4
-
-    pdf_files = [each for each in os.listdir(DOC_DIR) if each.endswith(".pdf")]
-    for pdf_file in tqdm(pdf_files):
-        pdf_path = os.path.join(DOC_DIR, pdf_file)
-        process_pdf(pdf_path, all_args)
-
-    # Create a pool of workers and distribute the tasks
-    print(f"Parallel processing over {max_workers} cores ...")
-    start_time = time.time()
-    with Pool(max_workers) as pool:
-        pool.starmap(process_page, all_args)
-    end_time = time.time()
-    print(f"Parallel processing took {end_time - start_time} seconds")
-    print("Done!")
+print("De-multiplexing Done!")
