568ea341a974ae5cf9784a0d02793d4643d4e92d
Rolando Garcia <rogarcia@berkeley.edu>
2025-10-11 15:35:04 -0500

    .

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import config
 
-from constants import DOC_DIR
+from .constants import DOC_DIR
 
 app = Flask(__name__)
 
